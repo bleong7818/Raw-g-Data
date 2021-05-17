2788e098c3342a828fb17fd944b990110e456767
Brandon Leong <bjtl1105@gmail.com>
2021-05-16 21:54:39 -0700

    removed more comments

--- src/data.js
+++ src/data.js
@@ -18,8 +18,6 @@ export const fetchAddedData = (number) => {
             data.results.forEach(game => {
                 nameArray.push(game.name);
             });
-            
-            
             let myChart = document.getElementById('myChart').getContext('2d');
             let massPopChart = new Chart(myChart, {
                 type: 'bar', //bar, horizontal bar, pie, line, doughnut, radar, polarArea
@@ -47,7 +45,6 @@ export const fetchAddedData = (number) => {
                     }
                 }
             });
-            
             const games = 
             `
                 <script class="chart">
@@ -60,31 +57,6 @@ export const fetchAddedData = (number) => {
             }).catch(err => {
             console.log(err);
         });
-        // .then(data => {
-            
-        //     console.log(data);
-        //     const half = data.results.splice(15, 19);
-        //     // 
-        //     resultsArray = [];
-        //     data.results.forEach(game => {
-        //     // 
-        //        resultsArray.push(game.added);
-        //     });
-        //     // console.log(resultsArray);
-            // const games = data.results.map(game => {
-            //     return `
-            // <li class="game" style="height:${game.added / 35}px; width:${game.added / 30}px">
-            //     <p class="image"> <img style="height:${game.added / 40}px; width:${game.added / 30}px" src="${game.background_image}" alt=${game.name}/> </p>
-            //     <p class ="game-text"> ${game.name} </p>
-            //     <p class ="game-text"> Added by ${game.added} users </p>
-            // </li>
-            // `
-            // }).join('');
-        //     document.getElementById('games-list')
-        //         .insertAdjacentHTML("afterbegin", games);
-        // }).catch(err => {
-        //     console.log(err);
-        // });
 };
 
 export const fetchMetacriticData = (number) => {
